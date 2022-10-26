5b4e3fd93800f0abd96857559e693af8e24ac16f
Harshita <mailmeharshita24@gmail.com>
2022-10-26 21:06:45 +0530

    Reverse number using method overloading

---
+++ MethodOverloading/src/ReverseNumber.java
@@ -0,0 +1,16 @@
+public class ReverseNumber {
+    public static int reverse(int n) {
+        int val = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            val = val * 10 + rem;
+            n = n / 10;
+
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(34567896));
+    }
+}
