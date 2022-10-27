bc26f0aedf1b2cc513e8bbac335a4397215cdf36
Harshita <mailmeharshita24@gmail.com>
2022-10-27 17:01:36 +0530

    Sum of the digits of a number.

--- MethodOverloading/src/SumNumber.java
+++ MethodOverloading/src/SumNumber.java
@@ -1,19 +1,14 @@
 import java.util.*;
 public class SumNumber {
-
     public static int printAdd(int n) {
-
         int val = 0, sum = 0;
-
         while (n != 0) {
             int rem = n % 10;
             // val = val * 10 + rem;
             sum = sum + rem;
             n = n / 10;
-
         }
         return sum;
-
     }
 
     public static void main(String[] args) {
