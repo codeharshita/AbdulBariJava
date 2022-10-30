1d5b06b3243aa9452513ffcd6ce60d830cdc00b2
Harshita <mailmeharshita24@gmail.com>
2022-10-30 18:32:13 +0530

    Garbage Collector

---
+++ Oops/src/TestGarbage.java
@@ -0,0 +1,14 @@
+
+public class TestGarbage {
+    public void finalize() {
+        System.out.println("Object is Garbage Collected.");
+    }
+
+    public static void main(String[] args) {
+        TestGarbage t1 = new TestGarbage();
+        TestGarbage t2 = new TestGarbage();
+        t1 = null;
+        t2 = null;
+        System.gc();
+    }
+}
