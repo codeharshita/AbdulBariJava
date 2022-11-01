12252ceca970c084e9228eb8228fba499cc94ac1
Harshita <mailmeharshita24@gmail.com>
2022-11-01 14:12:22 +0530

    while loop *

---
+++ Oops/src/whileLoop.java
@@ -0,0 +1,14 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 1, n = 10;
+
+        while (i <= n) {
+            //if(i==3)break;
+
+            System.out.println(i);
+            i++;
+
+        }
+
+    }
+}
