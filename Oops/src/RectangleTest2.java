3126c71f52d84cc335c64753f59150c858fb87d7
Harshita <mailmeharshita24@gmail.com>
2022-11-08 14:52:34 +0530

    data hiding

---
+++ Oops/src/RectangleTest2.java
@@ -0,0 +1,51 @@
+class Rectangle {
+    private int length;
+    private int breadth;
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public void setLength(int l) {
+        length = l;
+    }
+
+    public void setBreadth(int b) {
+        breadth = b;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+
+    public int peri() {
+        return 2 * (length + breadth);
+    }
+
+    public boolean isSquare() {
+        if (length == breadth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+public class RectangleTest2 {
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.setBreadth(2);
+        r.setLength(10);
+
+        System.out.println(r.getBreadth());
+        System.out.println(r.getLength());
+        System.out.println(r.area() + " is the area. ");
+        System.out.println(r.peri() + " is the perimeter.");
+    }
+}
