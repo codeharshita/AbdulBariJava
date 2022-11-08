1f7a8c5594dde28ab150ab17c6b963aa6deb17ee
Harshita <mailmeharshita24@gmail.com>
2022-11-08 14:04:59 +0530

    data hiding

---
+++ Oops/src/RectangleTest.java
@@ -0,0 +1,31 @@
+public class RectangleTest {
+
+    private int length;
+    public int breadth;
+
+    public int area() {
+        return length * breadth;
+    }
+
+    public int perimeter() {
+        return 2 * (length + breadth);
+    }
+
+    public boolean isSquare() {
+        if (length == breadth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        RectangleTest r = new RectangleTest();
+        r.length = 19;
+        r.breadth = 10;
+
+        System.out.println(r.area() + " is the area. ");
+        System.out.println(r.perimeter() + " is the perimeter.");
+        System.out.println(r.isSquare());
+    }
+}
