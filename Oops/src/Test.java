1f42615adc0fb415fc5dcfd92277604eaeca8e1d
Harshita <mailmeharshita24@gmail.com>
2022-11-10 08:13:42 +0530

    new commit

---
+++ Oops/src/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+    int a, b;
+
+    public static void m1() {
+        int x = 10;
+        System.out.println(x);
+    }
+
+    public static void m2() {
+        int m = 30;
+        System.out.println(m);
+    }
+
+    public static void m3() {
+        int z = 70;
+        System.out.println(z);
+    }
+
+    public static void main(String[] args) {
+        m1();
+        m2();
+        m3();
+    }
+}
