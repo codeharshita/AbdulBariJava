c3b15c7cbe73dab1b1b666586d75fa54d75a5237
Harshita <mailmeharshita24@gmail.com>
2022-11-22 12:54:19 +0530

    Interface Example

---
+++ Oops/src/InterfaceExample.java
@@ -0,0 +1,32 @@
+interface Test3 {
+    int X = 56;// this static and final.
+
+    void meth1();
+
+    void meth2();
+
+    public static void meth3() {
+        System.out.println("This is method 3.");
+    }
+}
+
+class MyTest implements Test3 {
+    @Override
+    public void meth1() {
+        System.out.println("This is the method 1");
+    }
+
+    @Override
+    public void meth2() {
+        System.out.println("This is the method 2");
+    }
+}
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        MyTest t = new MyTest();
+        t.meth1();
+        t.meth2();
+
+    }
+}
