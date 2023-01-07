dd95bc7c4b15778ea9b470d08508af4ee4a57c43
Harshita <mailmeharshita24@gmail.com>
2023-01-07 19:07:37 +0530

    new commit

---
+++ src/Comparison.java
@@ -0,0 +1,11 @@
+public class Comparison {
+    public static void main(String[] args) {
+
+
+        StudentNew st1 = new StudentNew();
+        StudentNew st2 = new StudentNew();
+
+        System.out.println(st1 == st2);
+        System.out.println(st1.equals(st1));
+    }
+}
