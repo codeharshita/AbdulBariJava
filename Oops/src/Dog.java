b8a05f79d2500c51ce768f227c7d6b291c79578e
Harshita <mailmeharshita24@gmail.com>
2023-01-14 14:59:00 +0530

    Understanding Constructors

---
+++ Oops/src/Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+
+    private Dog() {
+
+    }
+
+    String name;
+    int age;
+    int legs;
+
+    public Dog(String name, int age, int legs) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+    }
+
+}
--- Oops/src/Main.java
+++ Oops/src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person();
-        p.name = " Tom ";
-        p.age = 35;
+//        Person p = new Person();
+//        p.name = " Tom ";
+//        p.age = 35;
+//
+//        p.calcAge();
+//        p.eat();
+//        p.run();
 
-        p.calcAge();
-        p.eat();
-        p.run();
+
+        Dog dog = new Dog("Sheru", 12, 4);
+        System.out.println(dog.name + " " + dog.legs + " " + dog.age);
+
     }
 
 }
