5079534bd98b92f445eaf34209af9586bab41801
Harshita <mailmeharshita24@gmail.com>
2023-01-14 14:24:14 +0530

    String to int and int to String conversion

---
+++ Oops/src/Conversion.java
@@ -0,0 +1,14 @@
+//Convert String to int
+
+public class Conversion {
+    public static void main(String[] args) {
+        String s = "500";
+        int i = Integer.parseInt(s);
+        System.out.println(i);
+
+        int m = 90;
+        String k = Integer.toString(m);
+        System.out.println(k);
+    }
+}
+
