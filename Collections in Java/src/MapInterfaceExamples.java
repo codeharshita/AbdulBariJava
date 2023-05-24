bdd03b7214ee82c3bb97a68504f8307d37c86fbf
Harshita <mailmeharshita24@gmail.com>
2023-05-24 10:48:13 +0530

    CollectionFramework

--- Collections in Java/src/MapInterfaceExamples.java
+++ Collections in Java/src/MapInterfaceExamples.java
@@ -1,7 +1,22 @@
 import java.util.*;
 
 public class MapInterfaceExamples {
+
+    static void PrintHashTable() {
+        Hashtable ht = new Hashtable();
+        ht.put(1, "S");
+        ht.put(2, "D");
+        ht.put(2, "A");
+        ht.put(2, "B");
+        ht.put(2, "C");
+        System.out.println(3);
+        System.out.println(ht);
+
+
+    }
+
     public static void main(String[] args) {
+        PrintHashTable();
         LinkedHashMap<Integer, String> lhm = new LinkedHashMap<>(5);
         lhm.put(1, "A");
         lhm.put(2, "B");
